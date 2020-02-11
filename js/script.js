fea92d4749ec757e49ad4291e57ddb0b5efc3164
Jasper Tham <jaspertham98@gmail.com>
2020-02-11 19:40:39 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,88 @@
+$(document).ready(function() {
+    // navbar scroll effect start
+    var pageWidth = $(window).width();
+    // Transition effect for navbar 
+    $(window).scroll(function() {
+      // checks if window is scrolled more than 500px, adds/removes solid class
+      if($(this).scrollTop() > 20) { 
+          $('.navbar').addClass('solid');
+      } else {
+          $('.navbar').removeClass('solid');
+      }
+    });
+
+    // navbar scroll effect end
+
+    // typing animation start
+    var TxtType = function(el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 2000;
+        this.txt = '';
+        this.tick();
+        this.isDeleting = false;
+    };
+
+    TxtType.prototype.tick = function() {
+        var i = this.loopNum % this.toRotate.length;
+        var fullTxt = this.toRotate[i];
+
+        if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+
+        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+        var that = this;
+        var delta = 200 - Math.random() * 100;
+
+        if (this.isDeleting) { delta /= 2; }
+
+        if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+        }
+
+        setTimeout(function() {
+        that.tick();
+        }, delta);
+    };
+
+    window.onload = function() {
+        var elements = document.getElementsByClassName('typewrite');
+        for (var i=0; i<elements.length; i++) {
+            var toRotate = elements[i].getAttribute('data-type');
+            var period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+              new TxtType(elements[i], JSON.parse(toRotate), period);
+            }
+        }
+        // INJECT CSS
+        var css = document.createElement("style");
+        css.type = "text/css";
+        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+        document.body.appendChild(css);
+    };
+    // typing animation end
+
+    // progress bar animation start
+    $(window).on("scroll",function(){
+        $(".progress").each(function (index,progress){
+            var progressBar =  $(progress).find(".progress-bar");
+            var progressPercent = parseInt($(progressBar).attr("data-progress"));
+            if($(window).scrollTop()>$(progress).offset().top - 600){
+                $(progressBar)
+                .css("width",progressPercent + "%")
+                
+            }
+        });
+    });
+    // progress bar animation end
+});
